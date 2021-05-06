67cd029afe5c885d38f3090aafe171d5c2dc391c
Peter Brown <peter.brown@converse.edu>
2021-05-05 21:32:25 -0400

    Cleaned up typing

--- BinTree.py
+++ BinTree.py
@@ -10,18 +10,18 @@ class BinTree(Generic[T]):
     def _invariant(self) -> bool:
         """Class invariant, which actually validates the entire subtree."""
         # self._data is only None in the case of an empty tree
-        valid:bool = self._data is not None or (self._left is None and self._right is None) # type: ignore
-        if self._left is not None:                      # type: ignore
-            valid = valid and self._left._invariant()   # type: ignore
-        if self._right is not None:                     # type: ignore
-            valid = valid and self._right._invariant()  # type: ignore
+        valid:bool = self._data is not None or (self._left is None and self._right is None)
+        if self._left is not None:                      
+            valid = valid and self._left._invariant()   
+        if self._right is not None:                     
+            valid = valid and self._right._invariant()
         return valid
 
     def __init__(self, value:Optional[T]) -> None:
         """Constructs a node with no children and contents VALUE."""
         self._data:Optional[T] = value
-        self._left:Optional[BinTree] = None
-        self._right:Optional[BinTree] = None
+        self._left:Optional[BinTree[T]] = None
+        self._right:Optional[BinTree[T]] = None
         # Post:
         assert self._invariant()
 
@@ -30,39 +30,39 @@ class BinTree(Generic[T]):
     def data(self) -> Optional[T]:
         return self._data
 
-    def leftChild(self) -> Optional['BinTree']:
-        return self._left                        # type: ignore
+    def leftChild(self) -> Optional['BinTree[T]']:
+        return self._left                        
 
-    def rightChild(self) -> Optional['BinTree']:
-        return self._right                       # type: ignore
+    def rightChild(self) -> Optional['BinTree[T]']:
+        return self._right 
 
     def hasLeftChild(self) -> bool:
-        return (self._left is not None)          # type: ignore
+        return (self._left is not None)
 
     def hasRightChild(self) -> bool:
-        return (self._right is not None)         # type: ignore
+        return (self._right is not None)
 
     def __len__(self) -> int:
         """Return the number of nodes in the tree."""
         count = 1 # Current node
         if self.hasLeftChild():
-            count = count + len(self._left)      # type: ignore
+            count = count + len(cast(BinTree[T], self._left))
         if self.hasRightChild():
-            count = count + len(self._right)     # type: ignore
+            count = count + len(cast(BinTree[T], self._right))
         return count
 
     # ----------- Mutator methods -------------------
 
     def addLeft(self, value:T) -> None:
-        child:BinTree = BinTree(value)
+        child:BinTree[T] = BinTree[T](value)
         if self.hasLeftChild():
-            child._left = self._left             # type: ignore
+            child._left = self._left
         self._left = child
 
     def addRight(self, value:T) -> None:
-        child:BinTree = BinTree(value)
+        child:BinTree[T] = BinTree[T](value)
         if self.hasRightChild():
-            child._right = self._right           # type: ignore
+            child._right = self._right           
         self._right = child
     
 
--- TestBinTree.py
+++ TestBinTree.py
@@ -8,18 +8,18 @@ from BinTree import BinTree
 class TestBinTree(unittest.TestCase):
 
     def setUp(self) -> None:
-        self._1node = BinTree('a')                   #      a
+        self._1node:BinTree[str] = BinTree[str]('a')                   #      a
 
-        self._3nodes = BinTree('a')                  #      a      
+        self._3nodes:BinTree[str] = BinTree[str]('a')                  #      a      
         self._3nodes.addLeft('b')                    #     /  \
         self._3nodes.addRight('c')                   #    b    c
 
-        self._6nodes = BinTree('a')                                 #
+        self._6nodes:BinTree[str] = BinTree[str]('a')                                 #
         self._6nodes.addLeft('b')                                   #          a
         self._6nodes.addRight('c')                                  #       /      \
-        cast(BinTree, self._6nodes.leftChild()).addRight('d')       #    b          c
-        cast(BinTree, self._6nodes.rightChild()).addLeft('e')       #     \        / \
-        cast(BinTree, self._6nodes.rightChild()).addRight('f')      #      d      e   f
+        cast(BinTree[str], self._6nodes.leftChild()).addRight('d')       #    b          c
+        cast(BinTree[str], self._6nodes.rightChild()).addLeft('e')       #     \        / \
+        cast(BinTree[str], self._6nodes.rightChild()).addRight('f')      #      d      e   f
 
     def test_hasLeft(self) -> None:
         self.assertFalse(self._1node.hasLeftChild())
@@ -33,52 +33,69 @@ class TestBinTree(unittest.TestCase):
         self.assertEqual(self._1node.data(), 'a')
 
         self.assertEqual(self._3nodes.data(), 'a')
-        self.assertEqual(self._3nodes.leftChild().data(), 'b')
-        self.assertEqual(self._3nodes.rightChild().data(), 'c')
+        leftChild:BinTree[str] = cast(BinTree[str], self._3nodes.leftChild())
+        rightChild:BinTree[str] = cast(BinTree[str], self._3nodes.rightChild())
+        self.assertEqual(leftChild.data(), 'b')
+        self.assertEqual(rightChild.data(), 'c')
 
         self.assertEqual(self._6nodes.data(), 'a')
-        self.assertEqual(self._6nodes.leftChild().data(), 'b')
-        self.assertEqual(self._6nodes.rightChild().data(), 'c')
-        self.assertEqual(self._6nodes.leftChild().rightChild().data(), 'd')
-        self.assertEqual(self._6nodes.rightChild().leftChild().data(), 'e')
-        self.assertEqual(self._6nodes.rightChild().rightChild().data(), 'f')
+        leftChild = cast(BinTree[str], self._6nodes.leftChild())
+        rightChild = cast(BinTree[str], self._6nodes.rightChild())
+        self.assertEqual(leftChild.data(), 'b')
+        self.assertEqual(rightChild.data(), 'c')
+        left_rightChild:BinTree[str] = cast(BinTree[str], leftChild.rightChild())
+        right_leftChild:BinTree[str] = cast(BinTree[str], rightChild.leftChild())
+        right_rightChild:BinTree[str] = cast(BinTree[str], rightChild.rightChild())
+        self.assertEqual(left_rightChild.data(), 'd')
+        self.assertEqual(right_leftChild.data(), 'e')
+        self.assertEqual(right_rightChild.data(), 'f')
 
     def test_addLeft_noChild(self) -> None:
         self.assertFalse(self._1node.hasLeftChild())
         self._1node.addLeft('z')
         self.assertTrue(self._1node.hasLeftChild())
-        self.assertEqual(self._1node.leftChild().data(), 'z')
+        leftChild:BinTree[str] = cast(BinTree[str], self._1node.leftChild())
+        self.assertEqual(leftChild.data(), 'z')
 
     def test_addLeft_Child(self) -> None:
         self.assertTrue(self._3nodes.hasLeftChild())
-        self.assertEqual(self._3nodes.leftChild().data(), 'b')
-        self.assertFalse(self._3nodes.leftChild().hasLeftChild())
+        leftChild:BinTree[str] = cast(BinTree[str], self._3nodes.leftChild())
+        self.assertEqual(leftChild.data(), 'b')
+        self.assertFalse(leftChild.hasLeftChild())
+
         self._3nodes.addLeft('x')
         self.assertTrue(self._3nodes.hasLeftChild())
-        self.assertEqual(self._3nodes.leftChild().data(), 'x')
-        self.assertTrue(self._3nodes.leftChild().hasLeftChild())
-        self.assertEqual(self._3nodes.leftChild().leftChild().data(), 'b')
+        leftChild = cast(BinTree[str], self._3nodes.leftChild())
+        self.assertEqual(leftChild.data(), 'x')
+        self.assertTrue(leftChild.hasLeftChild())
+        left_leftChild:BinTree[str] = cast(BinTree[str], leftChild.leftChild())
+        self.assertEqual(left_leftChild.data(), 'b')
 
     def test_addRight_noChild(self) -> None:
         self.assertFalse(self._1node.hasRightChild())
         self._1node.addRight('y')
         self.assertTrue(self._1node.hasRightChild())
-        self.assertEqual(self._1node.rightChild().data(), 'y')
+        rightChild:BinTree[str] = cast(BinTree[str], self._1node.rightChild())
+        self.assertEqual(rightChild.data(), 'y')
 
     def test_addRight_Child(self) -> None:
         self.assertTrue(self._3nodes.hasRightChild())
-        self.assertEqual(self._3nodes.rightChild().data(), 'c')
-        self.assertFalse(self._3nodes.rightChild().hasRightChild())
+        rightChild:BinTree[str] = cast(BinTree[str], self._3nodes.rightChild())
+        self.assertEqual(rightChild.data(), 'c')
+        self.assertFalse(rightChild.hasRightChild())
+
         self._3nodes.addRight('w')
         self.assertTrue(self._3nodes.hasRightChild())
-        self.assertEqual(self._3nodes.rightChild().data(), 'w')
-        self.assertTrue(self._3nodes.rightChild().hasRightChild())
-        self.assertEqual(self._3nodes.rightChild().rightChild().data(), 'c')
+        rightChild = cast(BinTree[str], self._3nodes.rightChild())
+        self.assertEqual(rightChild.data(), 'w')
+        self.assertTrue(rightChild.hasRightChild())
+        right_rightChild:BinTree[str] = cast(BinTree[str], rightChild.rightChild())
+        self.assertEqual(right_rightChild.data(), 'c')
 
     def test_len(self) -> None:
-        self.assertEqual(len(self._1node), 1)
-        self.assertEqual(len(self._3nodes), 3)
-        self.assertEqual(len(self._6nodes), 6)
+        self.assertEqual(len(self._1node), 1)  
+        self.assertEqual(len(self._3nodes), 3)  
+        self.assertEqual(len(self._6nodes), 6) 
 
 if __name__ == '__main__':
     unittest.main()
